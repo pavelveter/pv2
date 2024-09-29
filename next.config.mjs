2e4526286aaf7e1581508091d49af7f925869edf
Borisov Pavel <kayjaylitee@gmail.com>
2024-09-30 01:44:50 +0300

    Changed next.config.mjs, added aliases for Webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
-};
+  webpack: config => {
+    // Настройка алиасов для Webpack
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
 
+    return config
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
